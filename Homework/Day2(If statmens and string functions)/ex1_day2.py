e226f4a916490dcc3afe7c9095499d1259be53bf
CHUREKGIT <chur@amu.edu.pl>
2019-03-04 21:31:23 +0100

    Game X or O Checker

--- Homework/Day2(If statmens and string functions)/ex1_day2.py
+++ Homework/Day2(If statmens and string functions)/ex1_day2.py
@@ -5,7 +5,7 @@ var = input("Give me 9 characters o or x")
 if var[0] == var[3] == var[6] or var[2] == var[5] == var[8]:
     print("WIN!!!!")
 # sprawdzam rząd
-elif var[0:3] =="xxx" or var[0:3] =="ooo" or var[3:5] =="xxx" or var[3:5] =="ooo" or var[6:8]=="xxx" or var[6:8]=="ooo":
+elif var[0:3] =="xxx" or var[0:3] =="ooo" or var[3:6] =="xxx" or var[3:6] =="ooo" or var[6:9]=="xxx" or var[6:9]=="ooo":
     print("WIN!!!")
 #sprawdzam skos
 elif var[0] == var[4] == var[8] or var[2] == var[4] == var[6]:
