395a4c60156e667aa40f22b85d13616822393791
CHUREKGIT <chur@amu.edu.pl>
2019-03-09 18:07:29 +0100

    Duplicated folder

--- Homework/Day1/ex1_day1.py
+++
@@ -1,7 +0,0 @@
-x = "Ala ma kota, a kot ma Ale"
-y = x.replace('Ala', 'Arek')
-y = y.replace('ma', 'woli', 1)
-y = y.replace('kota', 'psy')
-y = y.replace('kot', 'najbardziej')
-y = y.replace('ma', 'boksery')
-print(y[:-3])
