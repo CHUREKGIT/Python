791b3a8cb00da015c4aaf4ed28932a6ac161a00d
CHUREKGIT <chur@amu.edu.pl>
2019-03-09 18:06:24 +0100

    Duplicated folder

--- Homework/Day2(If statmens and string functions)/ex7_day2.py
+++
@@ -1,9 +0,0 @@
-month = input("Give me a month name")
-
-
-if month.upper() in ('SEPTEMBER','APRIL', 'JUNE', 'NOVEMBER'):
-    print("This month has 30 days")
-elif month.upper() == 'FEBRUARY':
-    print("This month has 28 days and 29 in leap year")
-else:
-    print('This month has 31 days')
